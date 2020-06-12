19b525ec15ad450c3a429710cb49019465ffea46
yinhaiying <yinhaiying@58.com>
2020-06-12 18:06:20 +0800

    登录组件的实现

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,13 +1,60 @@
 
 import React, { Component } from 'react';
 class Login extends Component {
-    render() {
-        return (
-            <div>
-                Login
+  constructor() {
+    super();
+    this.state = {
+      email: "",
+      password: "",
+      errors: {},
+    };
+  }
+  onChange = (e) => {
+    console.log(e.target.name);
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">登录</h1>
+              <p className="lead text-center">用正确的账号登录</p>
+              <form onSubmit = {this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Login;
